24e0bd005aec8f4bb021709ea7811d2c3dce2da1
Anton Pomozov <pomozoff@mail.ru>
2017-07-29 12:57:42 +0300

    add: iOS target

---
+++ AlamofireXMLRPC_iOS/AlamofireXMLRPC_iOS.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireXMLRPC_iOS.h
+//  AlamofireXMLRPC_iOS
+//
+//  Created by Anton Pomozov on 29/07/2017.
+//  Copyright © 2017 kodlian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamofireXMLRPC_iOS.
+FOUNDATION_EXPORT double AlamofireXMLRPC_iOSVersionNumber;
+
+//! Project version string for AlamofireXMLRPC_iOS.
+FOUNDATION_EXPORT const unsigned char AlamofireXMLRPC_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireXMLRPC_iOS/PublicHeader.h>
+
+
